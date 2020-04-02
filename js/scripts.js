7bc8a71424c3f4845126ab23a60e7a2cdcf829b1
shonburgundy <seanbeloussow@yahoo.com>
2020-04-02 11:11:35 -0700

    continue debugging to account for second condition
    coauthored by: Taylor Phillips <taylorphillips133@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,29 @@
-//UI
+//Business Interface
 var convertToRoman = function(num) { //function will convert roman numerals
-  var romanNumerals = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"]
-  var decimal = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000]
+  var romanNumeral = ["I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"];
+  var decimal = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
 
   var result = "";
 
-  for (var i = 0; i < decimal.length; i++)
+  for (var i = 0; i < decimal.length; i++) {
+    while (decimal[i] <= num) {
+    }
+  }
 }
+
+// if (!num.match(/[0-9]/g)) {
+//   return "These are not numbers.";
+
   
-console.log(num);
+//console.log();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function covertToRoman(num) {
-//   var roman = {
-// M: 1000,
-// CM: 900,
-// D: 500,
-// CD: 400,
-// C: 100,
-// XC: 90,
-// L: 50,
-// XL: 40,
-// X: 10,
-// IX: 9,
-// V: 5,
-// IV: 4,
-// I: 1
-
+//UI
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("input#userInput").val();
+    //var result = convertToRoman(num);
+    $('#output').text(convertToRoman(result));
+    console.log(result)
+  });
+});
